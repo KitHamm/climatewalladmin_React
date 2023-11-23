12c7fa9a2175c2a86942ceaec149e8d5c896de5b
Chris Perry <cmperry@live.co.uk>
2023-11-23 13:39:23 +0000

    -added rows for each day of approved and denied responses with count

--- src/components/Approved.js
+++ src/components/Approved.js
@@ -2,12 +2,23 @@ import { useQuery } from "@apollo/client";
 import { GET_APPROVED } from "./queries";
 import Empty from "./Empty";
 import Response from "./Response";
+import { useEffect, useState } from "react";
 
 export default function Approved() {
-    const dates = [];
+    const [testDates, setTestDates] = useState([]);
     const { loading, error, data } = useQuery(GET_APPROVED, {
         pollInterval: 1000,
     });
+    useEffect(() => {
+        for (let index = 0; index < testDates.length; index++) {
+            if (document.getElementById(testDates[index] + "-container")) {
+                document.getElementById(testDates[index] + "-count").innerHTML =
+                    document.getElementById(
+                        testDates[index] + "-container"
+                    ).childNodes.length;
+            }
+        }
+    }, [testDates]);
     if (loading)
         return (
             <>
@@ -31,6 +42,20 @@ export default function Approved() {
             </>
         );
     if (data) {
+        data.responses.data.forEach((response) => {
+            if (
+                !testDates.includes(
+                    response.attributes.createdAt.toString().split("T")[0]
+                )
+            ) {
+                setTestDates([
+                    ...testDates,
+                    response.attributes.createdAt.toString().split("T")[0],
+                ]);
+            }
+        });
+    }
+    if (testDates.length > 0) {
         return (
             <>
                 <div className="col-12 mt-5 mb-4">
@@ -72,68 +97,119 @@ export default function Approved() {
                     className="approved-container fade-out"
                     id="approved-container"
                     style={{ maxHeight: "0px" }}>
-                    {data.responses.data.length > 0 ? (
-                        data.responses.data.map((response, index) => {
-                            let dateElement = <></>;
-                            if (
-                                !dates.includes(
-                                    response.attributes.createdAt
-                                        .toString()
-                                        .split("T")[0]
-                                )
-                            ) {
-                                dates.push(
-                                    response.attributes.createdAt
-                                        .toString()
-                                        .split("T")[0]
-                                );
-                                dateElement = (
-                                    <div className="cw-date">
-                                        {
-                                            response.attributes.createdAt
-                                                .toString()
-                                                .split("T")[0]
-                                                .split("-")[2]
-                                        }
-                                        -
-                                        {
-                                            response.attributes.createdAt
-                                                .toString()
-                                                .split("T")[0]
-                                                .split("-")[1]
-                                        }
-                                        -
-                                        {
-                                            response.attributes.createdAt
-                                                .toString()
-                                                .split("T")[0]
-                                                .split("-")[0]
-                                        }
-                                    </div>
-                                );
-                            }
-                            return (
-                                <div key={response.id}>
-                                    {dateElement}
-                                    <Response
-                                        type="approved"
-                                        question={response.attributes.question}
-                                        response={response.attributes.response}
-                                        id={response.id}
-                                        user={response.attributes.approvedBy}
-                                        createdAt={
-                                            response.attributes.createdAt
-                                        }
-                                        updatedAt={
-                                            response.attributes.updatedAt
-                                        }
-                                    />
+                    {testDates.reverse().map((date) => {
+                        return (
+                            <div key={date}>
+                                <div
+                                    className="cw-date"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() => {
+                                        var el = document.getElementById(
+                                            date + "-container"
+                                        );
+                                        var containerEl =
+                                            document.getElementById(
+                                                "approved-container"
+                                            );
+                                        containerEl.style.maxHeight =
+                                            el.scrollHeight +
+                                            containerEl.scrollHeight +
+                                            1000 +
+                                            "px";
+                                        el.classList.contains("fade-out")
+                                            ? el.classList.replace(
+                                                  "fade-out",
+                                                  "fade-in"
+                                              )
+                                            : el.classList.replace(
+                                                  "fade-in",
+                                                  "fade-out"
+                                              );
+                                        el.style.maxHeight === "0px"
+                                            ? (el.style.maxHeight =
+                                                  el.scrollHeight + 1000 + "px")
+                                            : (el.style.maxHeight = "0px");
+                                    }}>
+                                    {
+                                        date
+                                            .toString()
+                                            .split("T")[0]
+                                            .split("-")[2]
+                                    }
+                                    -
+                                    {
+                                        date
+                                            .toString()
+                                            .split("T")[0]
+                                            .split("-")[1]
+                                    }
+                                    -
+                                    {
+                                        date
+                                            .toString()
+                                            .split("T")[0]
+                                            .split("-")[0]
+                                    }{" "}
+                                    (
+                                    <div
+                                        id={date + "-count"}
+                                        style={{
+                                            display: "inline-flex",
+                                        }}></div>
+                                    )
                                 </div>
-                            );
-                        })
-                    ) : (
-                        <Empty text="Nothing to show." />
-                    )}
+                                <div
+                                    id={date + "-container"}
+                                    className="date-response-container"
+                                    style={{ maxHeight: "0px" }}>
+                                    {data.responses.data.map(
+                                        (response, index) => {
+                                            if (
+                                                response.attributes.createdAt
+                                                    .toString()
+                                                    .split("T")[0] === date
+                                            ) {
+                                                return (
+                                                    <div key={response.id}>
+                                                        <Response
+                                                            type="approved"
+                                                            question={
+                                                                response
+                                                                    .attributes
+                                                                    .question
+                                                            }
+                                                            response={
+                                                                response
+                                                                    .attributes
+                                                                    .response
+                                                            }
+                                                            id={response.id}
+                                                            user={
+                                                                response
+                                                                    .attributes
+                                                                    .approvedBy
+                                                            }
+                                                            createdAt={
+                                                                response
+                                                                    .attributes
+                                                                    .createdAt
+                                                            }
+                                                            updatedAt={
+                                                                response
+                                                                    .attributes
+                                                                    .updatedAt
+                                                            }
+                                                        />
+                                                    </div>
+                                                );
+                                            }
+                                            return "";
+                                        }
+                                    )}
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </>
         );
